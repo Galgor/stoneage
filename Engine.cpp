6b2e8f702eb6c526886a30d009c03cd593dc904c
claudiobierig <claudiobierig@uni-oldenburg.de>
2016-04-02 15:32:19 +0200

    EngineNow

---
+++ Engine.cpp
@@ -0,0 +1,32 @@
+/*
+ * Engine.cpp
+ *
+ *  Created on: Apr 2, 2016
+ *      Author: claudio
+ */
+
+#include "Engine.h"
+#include "Move.h"
+#include "Interface.h"
+
+Engine::Engine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Engine::~Engine() {
+	// TODO Auto-generated destructor stub
+}
+
+Move* Engine::getMove() {
+	return m_pGame->getActivePlayer()->getMove(m_pInterface,m_pGame);
+}
+
+void Engine::run() {
+	while(! m_pGame->isFinished()){
+		Move* pMove = getMove();
+		if(m_pGame->isAllowed(pMove)){
+			m_pGame->evaluate(pMove);
+		}
+	}
+}
---
+++ Engine.h
@@ -0,0 +1,28 @@
+/*
+ * Engine.h
+ *
+ *  Created on: Apr 2, 2016
+ *      Author: claudio
+ */
+
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+class Interface;
+class Move;
+
+class Engine {
+
+private:
+	Interface* m_pInterface;
+	Game* m_pGame;
+
+public:
+	Engine();
+	virtual ~Engine();
+
+	Move* getMove();
+	void run();
+};
+
+#endif /* ENGINE_H_ */
